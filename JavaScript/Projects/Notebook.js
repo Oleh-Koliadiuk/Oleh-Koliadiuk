5420f1eb4bf4cd17512e0dece0d95ba1b8905e57
Oleg Koliadiuk <olegkoliadiukicloud.com@MacBook-Air-Oleg.local>
2026-05-13 22:43:20 +0300

    Worked in notebook.js

--- JavaScript/Projects/Notebook.js
+++ JavaScript/Projects/Notebook.js
@@ -201,3 +201,124 @@ let x = 1 && 2 ?? 3; // SyntaxError
 let x = (1 && 2) ?? 3; // Works
 
 alert(x); // 2
+
+
+
+// While
+let i = 0;
+
+while (i < 3) {
+    alert( i );
+    i++;
+}
+
+
+let i = 3
+
+while (i) {
+    alert(i);
+    i--;
+}
+
+
+let i = 3;
+
+while(i) alert(i--);
+
+
+
+do {
+    alert(i)
+    i++; // i + 1
+} while (i < 3)
+//
+
+// for
+for (let i = 0; i < 3; i++) {
+    alert(i);
+}
+//
+
+//
+let i = 0;
+
+for (; i < 3; i++){
+    alert(i)
+}
+//
+
+//
+let i = 0;
+
+for (; i < 3;) {
+    alert(i++);
+}
+//
+
+//
+for (;;) {}
+//
+
+//
+let sum = 0;
+
+while (true) {
+
+    let value = +prompt("Enter a number")   
+
+    if (!value) break;
+
+    sum +- value;
+
+}
+
+alert("Sum" + sum);
+//
+
+//
+for (let i = 0; i < 10; i++) {
+
+    if (i % 2 == 0) continue
+
+    alert(i); // 1, 3, 5, 7, 9
+}
+//
+
+//
+for (let i = 0; i < 10; i++) {
+    if (i % 2) {
+        alert(i);
+    }
+}
+//
+
+//
+if (i > 5) {
+    alert(i);
+} else {
+    continue;
+}
+
+// Syntax Error
+(i > 5) ? alert(i) : // continue; 
+//
+
+//
+outer: 
+    for (let i = 0; i < 3; i++) {
+
+        for (let j = 0; j < 3; j++) {
+
+        let input = prompt(`(${i},${j})`, '');
+
+        if (!input) break outer;
+
+  }
+}
+alert('Succes');
+//
+
+//
+label: {
+    break label;
+}
